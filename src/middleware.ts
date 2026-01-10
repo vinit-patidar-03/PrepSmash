bfbca4a80583ccc2c78056b01f22715f150305de
Vinit Patidar <Vinitpatidar780@gmail.com>
2026-01-10 14:08:54 +0530

    trial: testing middleware behaviour

--- src/middleware.ts
+++ src/middleware.ts
@@ -27,7 +27,7 @@ export const config = {
   matcher: [
     "/create",
     "/browse",
-    "/practice",
+    // "/practice",
     "/profile",
     "/playground/:path*",
     "/interview/feedback/:path*",
